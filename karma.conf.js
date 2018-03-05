c7e8ee1cfa10480fb46b79277f290188bab457d6
harttle <yangjvn@126.com>
2018-03-05 15:29:04 +0800

    fix: preprocessor for coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -44,7 +44,7 @@ module.exports = function (config) {
             // source files, that you wanna generate coverage for
             // do not include tests or libraries
             // (these files will be instrumented by Istanbul)
-            'index.js': ['coverage']
+            'src/**/*.js': ['coverage']
         },
 
         // test results reporter to use
